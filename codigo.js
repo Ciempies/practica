6866e1054961dfda4fd056b3117f8c5ddbb8e5fd
JhonatanPardo <38191596+Ciempies@users.noreply.github.com>
2024-06-11 14:32:14 -0500

    initial commit

---
+++ codigo.js
@@ -0,0 +1,15 @@
+// var pregunta=prompt("Como se dice hola en ingles")
+// if(pregunta=="Hello"){
+//     alert("Correcto")
+// }else if(pregunta=="Hi"){
+//     alert("Correcto")
+// }else{
+//     alert("Incorrecto")
+// }
+
+var puntaje=0
+
+function Saludar(){
+    var op=document.getElementsByName('hola').checked
+    alert(op)
+}
